0394f80417a065db3fb781f57925c70bc75459fa
matth <matth@BARAIBOBOX>
2024-09-11 14:31:38 -0400

    Final config tweek

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mainService = require("./api/emailerService");
 
 ex_app.use("/", mainService);
 
-ex_app.set("port", JSON.parse(process.env.PORT) || 4001);
+ex_app.set("port", JSON.parse(process.env.CURRENT_PORT) || 4001);
 
 ex_app.listen(ex_app.get("port"), ()=>{
     console.log("Listening on port " + process.env.CURRENT_PORT);
